1044e86f70d15288e86a87e794d20d3417e5de8c
Leon Yao <leon.yao@shopee.com>
2023-09-21 10:23:03 +0800

    add sft scripts

--- run_llama_sft.sh
+++ run_llama_sft.sh
@@ -2,19 +2,18 @@
 
 TRAIN=./data/train_dummy.csv
 VAL=./data/train_dummy.csv
-OUTPUT=./models/llama_7b_lora
+OUTPUT=./models/llama_7b_full_ft
 
-MASTER_ADDR=$MASTER_ADDR
-MASTER_PORT=$MASTER_PORT
-NNODES=$WORLD_SIZE
-GPUS_PER_NODE=1
-NODE_RANK=$RANK
+export CUDA_VISIBLE_DEVICES=1,2
+export OMP_NUM_THREADS=1
+MASTER_ADDR=localhost
+MASTER_PORT=6000
+NNODES=1
+GPUS_PER_NODE=2
+NODE_RANK=0
 
-
-log_path=./run_log/$AIP_RUN_ID
+log_path=./run_log/sft_1/
 mkdir -p $log_path
-
-
 
 torchrun --nproc_per_node $GPUS_PER_NODE \
   --nnodes $NNODES \
@@ -52,7 +51,6 @@ torchrun --nproc_per_node $GPUS_PER_NODE \
   --report_to tensorboard \
   --gradient_checkpointing True \
   --overwrite_cache False \
-  --use_lora \
   --overwrite_output_dir \
   --deepspeed ./code/zero_3.json  2>&1 | tee "$log_path/log_$RANK"
 
